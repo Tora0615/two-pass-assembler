f97e67ff446cf8229e7caae584c1de1e491872fd
Fe <82155803+FeHeap@users.noreply.github.com>
2021-06-11 00:58:21 +0800

    Base
    file open

--- twoPassAssembler.c
+++ twoPassAssembler.c
@@ -1,2 +1,31 @@
 # include <stdio.h>
 # include <stdlib.h>
+FILE *source_code,*opCode,*output;
+
+struct opCodeTable{
+	
+	
+	
+};
+
+int main(){
+	
+	char sourcefile[20] = "source.txt";
+	char opCodefile[20] = "opcode.txt";
+	
+	if((opCode = fopen(opCodefile,"r")) == NULL){
+		printf("Fail to open file %s !",opCodefile);
+		exit(0);
+	}
+	fclose(opCode);
+	
+	
+	if((source_code = fopen(sourcefile,"r")) == NULL){
+		printf("Fail to open file %s !",sourcefile);
+		exit(0);
+	}
+	fclose(source_code);
+	
+	system("PAUSE");
+	return 0;
+}
