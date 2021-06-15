d55fa1143c826e533801da8859f41c847ef1529c
Tora0615 <5438arthur@gmail.com>
2021-06-16 02:32:19 +0800

    [Info] Adding comments

--- twoPassAssembler.c
+++ twoPassAssembler.c
@@ -6,6 +6,44 @@
 FILE *source_code,*opCode,*output,*outputSP,*OBJ;
 
 
+//  資料片段與說明 
+
+//  source.txt
+//
+//  欄位 :  
+//  symbol	opCode	Input
+//
+//  FIRST	STL		RETADR
+//  CLOOP	JSUB	RDREC
+//  		LDA		LENGTH
+//  		COMP	ZERO
+//  		JEQ		ENDFIL
+
+
+//  opcode.txt
+//
+//  欄位 
+//  opcode  對應代碼
+// 
+//  ADD 	18
+//  ADDF 	58
+//  ADDR 	90
+//  AND 	40
+//  CLEAR 	B4
+//  COMP 	28
+//  COMPF 	88
+
+
+//  alphtable 
+//
+//  A : ADD 18 -> ADDF 58 -> ...
+//  B : 
+//  C : CLEAR B4 -> COMP 28 -> ...
+//  ↓
+//  ... 
+
+
+//宣告 opcode 小單元的結構 
 typedef struct opCodeUnit opCodeUnit;
 struct opCodeUnit{
 	char read[8];
@@ -14,12 +52,16 @@ struct opCodeUnit{
 };
 opCodeUnit **alphtable;
 
+
+//宣告 symbleUnit 小單元的結構 
 typedef struct symbleUnit symbleUnit;
 struct symbleUnit{
 	char read[16];
 	int address;
 };
 
+
+//初始宣告
 void setOpCode(opCodeUnit*,char*);
 void pass1();
 void getInst(char*,char*,char*,char*);
@@ -27,81 +69,16 @@ int stringX16ToInt(char*);
 int stringX10ToInt(char*);
 int commentLine(char*);
 void push(char*);
-opCodeUnit* getopCodeD(char*); 
-
 void pass2();
 void obj();
 void add(char*,int,int,int,char);
 void deletes();
+opCodeUnit* getopCodeD(char*); 
 
-int main(){
-	
-	char sourcefile[20] = "source.txt";
-	char opCodefile[20] = "opcode.txt";
-	
-	if((opCode = fopen(opCodefile,"r")) == NULL){
-		printf("Fail to open file %s !",opCodefile);
-		exit(0);
-	}
-	
-	alphtable = (opCodeUnit**)malloc(sizeof(opCodeUnit*)*ALPHABET);
-	int i;
-	for(i = 0;i < ALPHABET;i++){
-		*(alphtable+i) =  NULL;
-	}
-	
-	char charBuf = NULL;
-	char stringBuf[15];
-	
-	while(charBuf != EOF){ //store the data which are in opCodefile into alphtable
-		fscanf(opCode,"%[^\n]",stringBuf);
-		int index = (stringBuf[0] >= 'a' && stringBuf[0] <= 'z')?stringBuf[0]-'a':stringBuf[0]-'A';		
-		if(*(alphtable + index) == NULL){
-			*(alphtable + index) = (opCodeUnit*)malloc(sizeof(opCodeUnit));
-			opCodeUnit* point = *(alphtable + index);
-			setOpCode(point,stringBuf);
-		}
-		else{
-			opCodeUnit* point = *(alphtable + index);
-
-			while(point->next != NULL){
-				point = point->next;
-			}
-			point->next =  (opCodeUnit*)malloc(sizeof(opCodeUnit));
-			point = point->next;
-			setOpCode(point,stringBuf);
-		}
-		charBuf = fgetc(opCode);
-	}
-	
-	fclose(opCode);
-	
-	
-	
-	if((source_code = fopen(sourcefile,"r")) == NULL){
-		printf("Fail to open file %s !",sourcefile);
-		exit(0);
-	}
-	pass1();
-	
-	
-	fclose(source_code);
-	
-	if((source_code = fopen(sourcefile,"r")) == NULL){
-		printf("Fail to open file %s !",sourcefile);
-		exit(0);
-	}
-	pass2();
-	
-	
-	fclose(source_code);
-   	
-	system("PAUSE");
-	return 0;
-}
-
-void setOpCode(opCodeUnit* point,char* readin){
-	point->next = NULL;
+//------ main 用到的函數 --------- 
+//把 opCode 跟對照碼存入字母表的函數 
+void setOpCode(opCodeUnit* point, char* readin){  //readin 接收 stringBuf (一整行) 
+	point->next = NULL; //下一個先指向null 
 	int i;
 	for(i = 0;readin[i] != ' ';i++){
 		point->read[i] = readin[i];
@@ -113,16 +90,81 @@ void setOpCode(opCodeUnit* point,char* readin){
 	point->translate[2] = '\0';
 }
 
-int STSize = 2,TOP = 0;
-symbleUnit* symbleTable;
-int location,startAD;
 
-char ProgramName[30];
+int main(){
+	
+	char sourcefile[20] = "source.txt";
+	char opCodefile[20] = "opcode.txt";
+	
+	//開檔及檢查 
+	if((opCode = fopen(opCodefile,"r")) == NULL){
+		printf("Fail to open file %s !",opCodefile);
+		exit(0);
+	}
+	
+	
+	//宣告26個字母的指令表開頭 
+	alphtable = (opCodeUnit**)malloc(sizeof(opCodeUnit*)*ALPHABET);
+	int i;
+	for(i = 0;i < ALPHABET;i++){
+		*(alphtable+i) =  NULL;
+	}
+	char charBuf = NULL;
+	char stringBuf[15];  
+	//讀取opCodefile完成指令表 (見開頭 alphtable ) 
+	while(charBuf != EOF){ 
+		fscanf(opCode,"%[^\n]",stringBuf);  //一次讀到換行符號前，並存在stringBuf (像這樣 FIRST STL RETADR)
+		int index = (stringBuf[0] >= 'a' && stringBuf[0] <= 'z')?stringBuf[0]-'a':stringBuf[0]-'A';	 //區分大小寫並回傳 index	
+		if(*(alphtable + index) == NULL){ //index不存在 
+			*(alphtable + index) = (opCodeUnit*)malloc(sizeof(opCodeUnit));
+			opCodeUnit* point = *(alphtable + index);
+			setOpCode(point,stringBuf); //opCode 跟對照碼存入字母表 
+		}
+		else{  //曾經存過，index存在 
+			opCodeUnit* point = *(alphtable + index);
+			while(point->next != NULL){  //找到最末端位置 
+				point = point->next;
+			}
+			point->next =  (opCodeUnit*)malloc(sizeof(opCodeUnit));
+			point = point->next;
+			setOpCode(point,stringBuf); //opCode 跟對照碼存入字母表 
+		}
+		charBuf = fgetc(opCode); //讀一個字元，目的是為了把換行符號(\n)吃掉 
+	}
+	fclose(opCode);
+	
+	
+	//sourcefile開檔並處理pass1部分 
+	if((source_code = fopen(sourcefile,"r")) == NULL){
+		printf("Fail to open file %s !",sourcefile);
+		exit(0);
+	}
+	pass1();
+	fclose(source_code);
+	
+	
+	//sourcefile開檔並處理pass2部分 
+	if((source_code = fopen(sourcefile,"r")) == NULL){
+		printf("Fail to open file %s !",sourcefile);
+		exit(0);
+	}
+	pass2();
+	fclose(source_code);
+   	
+	system("PAUSE");
+	return 0;
+}
+
+
+
+int STSize = 2, TOP = 0;
+int location, startAD=0;
+symbleUnit* symbleTable;
 int Length;
 
 void pass1(){
-	char strBuf[100];
-	char charBuf;
+	char strBuf[100]; //symbol opCode Input (不含 \n )
+	char charBuf; // 吃掉 \n 用的 
 	char symbolBuf[16];
 	char opCodeBuf[8];
 	char InputBuf[16];
@@ -133,7 +175,7 @@ void pass1(){
 	}while(commentLine(strBuf));
 	printf("Instruction and it's location counter:\n\n");
 	getInst(strBuf,symbolBuf,opCodeBuf,InputBuf);
-	int i,j;
+	int i;
 	if(!stricmp(opCodeBuf,"START")){
 		startAD = stringX16ToInt(InputBuf);
 		location = startAD;
@@ -193,7 +235,6 @@ void pass1(){
 	}
 	printf("\t%s\n",strBuf);
 	Length = location-startAD;
-	//printf("%X\n",Length);
 	printf("\n\n\n");
 	printf("symbol table:\n\n");
 	for(i = 0;i < TOP;i++){
@@ -202,8 +243,9 @@ void pass1(){
 	printf("\n\n\n");
 }
 
+
+// push pass1
 void push(char* symbolBuf){
-	
 	int i;
 	for(i = 0;i < TOP;i++){
 		if(!stricmp((symbleTable+i)->read,symbolBuf)){
@@ -211,7 +253,6 @@ void push(char* symbolBuf){
 			exit(0);
 		}
 	}
-	
 	if(TOP == STSize){
 		STSize *= 2;
 		symbleUnit* temp = (symbleUnit*)malloc(STSize*sizeof(symbleUnit));
@@ -221,13 +262,12 @@ void push(char* symbolBuf){
 		}
 		symbleTable = temp;
 	}
-	
 	strcpy((symbleTable+TOP)->read,symbolBuf);
 	(symbleTable+TOP)->address = location;
 	TOP++;
 }
 
-
+// pass 1 & 2
 int commentLine(char* Line){
 	int i;
 	for(i = 0;i < strlen(Line);i++){
@@ -243,37 +283,61 @@ int commentLine(char* Line){
 }
 
 
-
+// 輸入 strBuf (只有一行) 輸出成 symbolBuf、opCodeBuf、InputBuf 字串 
 void getInst(char* strBuf,char* symbolBuf,char* opCodeBuf,char* InputBuf){
-	int i = 0,j;
-	for(j = 0;strBuf[i] != '\t'&&strBuf[i] != ' ';i++,j++){
+	
+	
+	// ↓下方區域開頭 i 所在位置
+	// FIRST    STL    RETADR 
+	// i
+	// 取得symbolBuf 
+	int i = 0,j; //j每次都會歸零  
+	for(j = 0; strBuf[i] != '\t'&&strBuf[i] != ' '; i++,j++){ //不為tab、空格，繼續執行 
 		symbolBuf[j] = strBuf[i];
 	}
-	symbolBuf[j] = '\0';
+	symbolBuf[j] = '\0';   //'\0' 空字元，用於字串的結束
 	
-	for(;;i++){
-		if((strBuf[i] != '\t'&&strBuf[i] != ' ')&&(strBuf[i-1] == '\t'||strBuf[i-1] == ' ')){
+	
+	// ↓下方區域開頭 i 所在位置
+	// FIRST    STL    RETADR 
+	//      i
+	
+	//略過空格、tab用 
+	for(;;i++){  //一直往下讀 
+		if((strBuf[i] != '\t'&&strBuf[i] != ' ')&&(strBuf[i-1] == '\t'||strBuf[i-1] == ' ')){  //直到讀到此處不為tab也不為空格，且其前方是 tab or 空格
 			break;
 		}
 	}
 	
-	for(j = 0;strBuf[i] != '\t'&&strBuf[i] != ' '&&strBuf[i] != '\0';i++,j++){
+	
+	// ↓下方區域開頭 i 所在位置
+	// FIRST    STL    RETADR 
+	//          i
+	
+	// 取得opCodeBuf 
+	for(j = 0;strBuf[i] != '\t'&&strBuf[i] != ' '&&strBuf[i] != '\0';i++,j++){ //不為tab、空格、字串尾端，繼續執行 
 		opCodeBuf[j] = strBuf[i];
 	}
 	opCodeBuf[j] = '\0';
 	
 	
+	
+	//略過空格、tab用 
 	for(;;i++){
 		if((strBuf[i] != '\t'&&strBuf[i] != ' ')&&(strBuf[i-1] == '\t'||strBuf[i-1] == ' ')){
 			break;
 		}
 	}
-	for(j = 0;strBuf[i] != '\t'&&strBuf[i] != ' '&&strBuf[i] != '\0'&&(strBuf[i] != '\t'&&strBuf[i] != ' '&&strBuf[i] != '\0');i++,j++){
+	
+	
+	// 取得InputBuf
+	for(j = 0;strBuf[i] != '\t'&&strBuf[i] != ' '&&strBuf[i] != '\0'&&(strBuf[i] != '\t'&&strBuf[i] != ' '&&strBuf[i] != '\0');i++,j++){ //不為tab、空格、字串尾端，繼續執行  
 		InputBuf[j] = strBuf[i];
 	}
 	InputBuf[j] = '\0';	
 }
 
+//十六進位字串轉 int，並回傳 
 int stringX16ToInt(char* InputBuf){
 	int Plus = 0;
 	int count = strlen(InputBuf) - 1;
@@ -294,6 +358,7 @@ int stringX16ToInt(char* InputBuf){
 	return Plus;
 }
 
+//十進位字串轉 int，並回傳 
 int stringX10ToInt(char* InputBuf){
 	int num = 0;
 	int count = strlen(InputBuf) - 1;
@@ -336,6 +401,12 @@ opCodeUnit* getopCodeD(char* opCodeBuf){
 	return point;
 }
 
+
+
+
+
+
+
 int rear = 0;
 
 typedef struct link link;
@@ -347,6 +418,9 @@ struct link{
 	char Cflag;
 };
 link* queue;
+
+
+
 void add(char* opCTrans,int Xlocation,int LOCAT,int add,char Cflag){
 	strcpy((queue+rear)->opCTrans,opCTrans);
 	(queue+rear)->Xlocation = Xlocation;
@@ -355,9 +429,17 @@ void add(char* opCTrans,int Xlocation,int LOCAT,int add,char Cflag){
 	(queue+rear)->Cflag = Cflag;
 	rear++;
 }
-void deletes(){
-	
-}
+
+
+
+
+
+
+
+char ProgramName[30];
+
+
+
 void pass2(){
 	outputSP = fopen("source_program.txt","w");
 	char strBuf[100];
@@ -390,11 +472,9 @@ void pass2(){
 	}while(commentLine(strBuf));
 	getInst(strBuf,symbolBuf,opCodeBuf,InputBuf);
 
-	int now = location;
 	queue = (link*)malloc(sizeof(link)*contain);
 
 	while(stricmp(opCodeBuf,"END")){
-		int prelo = location;
 		point = getopCodeD(opCodeBuf);
 		if (point == NULL){
 			strcpy(opCTrans,"");
@@ -456,7 +536,7 @@ void pass2(){
 				addLocate += 32768;
 				fprintf(outputSP,"%04X\n",addLocate);
 			}
-			add(opCTrans,addLocate,location,3,'E');
+			add(opCTrans,addLocate,location,3,'E'); //else +=3
 		}
 
 
@@ -481,7 +561,6 @@ void pass2(){
 			location += (3*num);
 		}
 		else{
-			opCodeUnit* point = getopCodeD(opCodeBuf);
 			location += 3;
 		}
 
@@ -495,9 +574,11 @@ void pass2(){
 	fclose(outputSP);
 	obj();
 }
+
+//objcode 生成 (2) 
 void obj(){
 	OBJ = fopen("objCode.txt","w");
-	fprintf(OBJ,"H%s\t%06X%06X\n",ProgramName,startAD,Length);
+	fprintf(OBJ,"H%s\t%06X%06X\n",ProgramName,startAD,Length); //h part  
 	int i,j;
 	int sum = 0;
 	int start = (queue+0)->LOCAT;
@@ -540,6 +621,6 @@ void obj(){
 			break;
 		}
 	}
-	fprintf(OBJ,"\n");
-	fprintf(OBJ,"E%06X\n",startAD);
+	fprintf(OBJ,"\n"); //t part 
+	fprintf(OBJ,"E%06X\n",startAD); //e part
 }
